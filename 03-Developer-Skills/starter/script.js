5dbf8179b3ee9fafa08c34fae76c3e1164b1484d
donkeyjot <ondrej.sebl@icloud.com>
2021-03-17 13:26:47 +0100

    save progress

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,7 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+// "use strict";
 
-let birthYear = 1990;
+// let birthYear = 1990;
 
-const calcAge = birthYear => 2037 - birthYear;
-//BUG
+// const calcAge = birthYear => 2037 - birthYear;
+// //BUGx:x
+
+// console.log('Hello there!');
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degrees celsius:")),
+  };
+
+  console.log(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
